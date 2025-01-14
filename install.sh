6268d9816da7a7b6d57c6c01167b98dc8f463419
Johannes Umander <johannes.umander@ume.se>
2025-01-14 21:15:19 +0100

    Only update boot order if it is required

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ fi
 echo -n "Install force boot ubuntu? (y/N) "
 read answer
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+	sudo ln -sf $CURRENT_DIR/boot-ubuntu-first.sh /usr/local/bin
 	sudo ln -sf $CURRENT_DIR/boot-ubuntu-first.service /etc/systemd/system
 	sudo systemctl enable boot-ubuntu-first.service
 	sudo systemctl start boot-ubuntu-first.service
