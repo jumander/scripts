32e42c4900272c33e98249cfd82bc951c643c914
Johannes Umander <johannes.umander@ume.se>
2025-04-21 22:47:28 +0200

    Fix ubuntu boot order script from failing

--- boot-ubuntu-first.sh
+++ boot-ubuntu-first.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if ["$(efibootmgr | grep -oP '^BootOrder: \K\d+')" != "0001"]; then
+if [[ "$(efibootmgr | grep -oP '^BootOrder: \K\d+')" != "0001" ]]; then
   efibootmgr -o 0001,0000
 fi
